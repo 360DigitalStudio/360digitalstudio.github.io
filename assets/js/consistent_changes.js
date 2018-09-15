be5c6ce6e7dbbaed8c20ad2fc7b686897de48062
Dash <dash.elhauge@gmail.com>
2018-09-15 05:13:35 -0400

    Added photo gallery; updated info on INDEX PAGE navbar for getting in touch

--- assets/js/consistent_changes.js
+++ assets/js/consistent_changes.js
@@ -1,28 +1,17 @@
 $('#menu').append(`<ul>
 										<li><a href="index.html">Homepage</a></li>
-										<li><a href="generic.html">Generic</a></li>
+										<li><a href="online_consultations.html">Online Consultations</a></li>
+
+										<li><a href="#">About us</a></li>
+										<li><a href="#">Proposals</a></li>
+										<li><a href="#">Reports</a></li>
+										<li><a href="#">Design</a></li>
+										<li>
+											<span class="opener">Job Applications</span>
+											<ul>
+												<li><a href="#">CRAP</a></li>
+												<li><a href="#">Doc Design</a></li>
+											</ul>
+										</li>
 										<li><a href="elements.html">Elements</a></li>
-										<li>
-											<span class="opener">Submenu</span>
-											<ul>
-												<li><a href="#">Lorem Dolor</a></li>
-												<li><a href="#">Ipsum Adipiscing</a></li>
-												<li><a href="#">Tempus Magna</a></li>
-												<li><a href="#">Feugiat Veroeros</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Etiam Dolore</a></li>
-										<li><a href="#">Adipiscing</a></li>
-										<li>
-											<span class="opener">Another Submenu</span>
-											<ul>
-												<li><a href="#">Lorem Dolor</a></li>
-												<li><a href="#">Ipsum Adipiscing</a></li>
-												<li><a href="#">Tempus Magna</a></li>
-												<li><a href="#">Feugiat Veroeros</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Maximus Erat</a></li>
-										<li><a href="#">Sapien Mauris</a></li>
-										<li><a href="#">Amet Lacinia</a></li>
 									</ul>`)
