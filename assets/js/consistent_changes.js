d50f0172ed6271fe38abdbe2f5128bc59b46d3b6
Dash <dash.elhauge@gmail.com>
2018-09-22 02:12:48 -0400

    Fixed navbar

--- assets/js/consistent_changes.js
+++ assets/js/consistent_changes.js
@@ -1,17 +1,42 @@
 $('#menu').append(`<ul>
 										<li><a href="index.html">Homepage</a></li>
+										<li><a href="about_us.html">About us</a></li>
 										<li><a href="online_consultations.html">Online Consultations</a></li>
+										<li>
+											<span class="opener">Design</span>
+												<ul>
+													<li><a href="#">Doc Design</a></li>
+													<li><a href="#">CARP</a></li>
+												</ul>
+										</li>
 
-										<li><a href="#">About us</a></li>
-										<li><a href="#">Proposals</a></li>
-										<li><a href="#">Reports</a></li>
-										<li><a href="#">Design</a></li>
 										<li>
 											<span class="opener">Job Applications</span>
 											<ul>
-												<li><a href="#">CRAP</a></li>
-												<li><a href="#">Doc Design</a></li>
+												<li><a href="#">Resume</a></li>
+												<li><a href="#">Cover Letter</a></li>
 											</ul>
 										</li>
+										<li>
+											<span class="opener">Reports </span>
+												<ul>
+													<li><a href="#">Progress</a></li>
+													<li><a href="#">Recommendation</a></li>
+													<li><a href="#">Field / Informational</a></li>
+												</ul>
+										</li>
+
+										<li>
+											<span class="opener">Proposals</span>
+												<ul>
+													<li><a href="#">Goods & Services</a></li>
+													<li><a href="#">Research</a></li>
+												</ul>
+										</li>
+										
+										
+										<li><a href="about_us.html">Collaboration</a></li>
+										<li><a href="about_us.html">How-To Videos</a></li>
+										<li><a href="about_us.html">Helpful Links</a></li>
 										<li><a href="elements.html">Elements</a></li>
 									</ul>`)
